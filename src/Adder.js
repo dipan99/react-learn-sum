ceb67c06c28a3ff4c71d5fd87069978bab03cf00
dipan99 <dipanbag99@gmail.com>
2021-03-11 11:05:02 +0530

    2 files modified

--- src/Adder.js
+++ src/Adder.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Adder = () => {
 
+  const [firstNum, setFirstNum] = useState(0);
+  const [secondNum, setSecondNum] = useState(0);
+  const [showResult, SetShowResult] = useState(false);
+
+  const triggerFirstNum = (e) => {
+    console.log('triggerFirstNum called.');
+    setFirstNum(e.target.value);
+    SetShowResult(true);
+  }
+
+  const triggerSecondNum = (e) => {
+    console.log('triggerSecondNum called.');
+    setFirstNum(e.target.value);
+    SetShowResult(true);
+  }
+
   return (
     <div>
-      <input placeholder="Num1" />
-      <input placeholder="Num2" />
-      <input />
+      <input placeholder="Num1" onInput={triggerFirstNum}/>
+      <input placeholder="Num2" onInput={(e) => {setSecondNum(e.target.value)}}/>
+      <input value={parseInt(firstNum)+parseInt(secondNum)}/>
+      {showResult && <p>Result: {firstNum} + {secondNum} = {firstNum+secondNum} </p>}
     </div>
   )
 }
--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import Adder from "./Adder";
 import "./style.css";
 
 export default function App() {
+  const [firstNum, setFirstNum] = useState(0);
+  const [secondNum, setSecondNum] = useState(0);
+  const [showResult, setShowResult] = useState(false);
+
+  const toggleAddition = () => {
+    console.log("toggleAddition called.");
+    setShowResult(true);
+  };
+
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+    <>
+      <Adder onToggle={toggleAddition} /> 
+      {showResult && <p>{firstNum} + {secondNum} = {firstNum+secondNum}</p>}
+    </>
+    );
 }
