1989d15ada14c45ecfd0d039b92f422bb91f80ba
dipan99 <dipanbag99@gmail.com>
2021-03-11 12:09:25 +0530

    2 files modified

--- src/Adder.js
+++ src/Adder.js
@@ -2,28 +2,35 @@ import React, {useState} from 'react';
 
 const Adder = () => {
 
-  const [firstNum, setFirstNum] = useState(0);
-  const [secondNum, setSecondNum] = useState(0);
-  const [showResult, SetShowResult] = useState(false);
+  const [firstNum, setFirstNum] = useState('');
+  const [secondNum, setSecondNum] = useState('');
+  //const [showResult, SetShowResult] = useState(false);
 
   const triggerFirstNum = (e) => {
     console.log('triggerFirstNum called.');
     setFirstNum(e.target.value);
-    SetShowResult(true);
+    //SetShowResult(true);
   }
 
   const triggerSecondNum = (e) => {
     console.log('triggerSecondNum called.');
-    setFirstNum(e.target.value);
-    SetShowResult(true);
+    setSecondNum(e.target.value);
+    //SetShowResult(true);
+  }
+
+  const clearFields = () => {
+    setFirstNum('');
+    setSecondNum('');
+    //SetShowResult(false);
   }
 
   return (
     <div>
-      <input placeholder="Num1" onInput={triggerFirstNum}/>
-      <input placeholder="Num2" onInput={(e) => {setSecondNum(e.target.value)}}/>
-      <input value={parseInt(firstNum)+parseInt(secondNum)}/>
-      {showResult && <p>Result: {firstNum} + {secondNum} = {firstNum+secondNum} </p>}
+      <input placeholder="Num1" value={firstNum} onInput={(e) => setFirstNum(e.target.value)}/>
+      <input placeholder="Num2" value={secondNum} onInput={(e) => setSecondNum(e.target.value)}/>
+      <input placeholder="Result..." value={firstNum && secondNum && parseInt(firstNum)+parseInt(secondNum)}/>
+      {firstNum && secondNum && <p>Result: {firstNum} + {secondNum} = {parseInt(firstNum)+parseInt(secondNum)} </p>}
+      {firstNum && secondNum && <button onClick={clearFields}>Clear</button>}
     </div>
   )
 }
--- src/App.js
+++ src/App.js
@@ -3,19 +3,12 @@ import Adder from "./Adder";
 import "./style.css";
 
 export default function App() {
-  const [firstNum, setFirstNum] = useState(0);
-  const [secondNum, setSecondNum] = useState(0);
-  const [showResult, setShowResult] = useState(false);
-
-  const toggleAddition = () => {
-    console.log("toggleAddition called.");
-    setShowResult(true);
-  };
+  
 
   return (
     <>
-      <Adder onToggle={toggleAddition} /> 
-      {showResult && <p>{firstNum} + {secondNum} = {firstNum+secondNum}</p>}
+      <Adder  /> 
+      
     </>
     );
 }
