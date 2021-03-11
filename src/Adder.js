2003ddf188d3a6dbb065a1f2171db539b4a772a6
dipan99 <dipanbag99@gmail.com>
2021-03-11 10:34:17 +0530

    1 file added

---
+++ src/Adder.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Adder = () => {
+
+  return (
+    <div>
+      <input placeholder="Num1" />
+      <input placeholder="Num2" />
+      <input />
+    </div>
+  )
+}
+
+export default Adder;
